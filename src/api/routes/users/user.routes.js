9ad59f55d4ce7d0732a75240cec9d7cde8588102
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-04-23 13:57:01 -0500

    feat(createUserRequest): add user request validations

--- src/api/routes/users/user.routes.js
+++ src/api/routes/users/user.routes.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
+const { validationResult } = require('express-validator');
+const CreateUserRequest = require('../../requests/CreateUserRequest');
 
 module.exports = ({ userController }) => {
   const router = Router();
 
   router.get('/', userController.index);
+  router.post('/', CreateUserRequest, (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).send({ errors: errors.array() });
+    }
+
+    return response.send(request.body);
+  });
 
   return router;
 };
