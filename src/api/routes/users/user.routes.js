ec0b52b5fd8f3d76c32ab2e21c96c0998e47d8a8
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-04-23 14:02:05 -0500

    chore(userRoute): uncapitalize createUserRequest

--- src/api/routes/users/user.routes.js
+++ src/api/routes/users/user.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { validationResult } = require('express-validator');
-const CreateUserRequest = require('../../requests/CreateUserRequest');
+const createUserRequest = require('../../requests/CreateUserRequest');
 
 module.exports = ({ userController }) => {
   const router = Router();
 
   router.get('/', userController.index);
-  router.post('/', CreateUserRequest, (request, response) => {
+  router.post('/', createUserRequest, (request, response) => {
     const errors = validationResult(request);
     if (!errors.isEmpty()) {
       return response.status(400).send({ errors: errors.array() });
