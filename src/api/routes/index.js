c9d48aaff1e8206de40287e78fa06d1e154b1607
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-04-23 11:31:46 -0500

    feat(router): add default route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -11,8 +11,21 @@ module.exports = ({ userRoutes, bookRoutes }) => {
     .use(json())
     .use(compression());
 
+  apiRouter.use('/', (request, response) => {
+    response.send({
+      version: '0.1.0-snapshot',
+      documentation: 'https://api.carlosarroyo.com/docs',
+      author: 'carlosarroyoam',
+    });
+  });
   apiRouter.use('/user', userRoutes);
   apiRouter.use('/book', bookRoutes);
+  apiRouter.get('*', (req, res) => {
+    res.json({
+      status: 'Not Found',
+      message: `The ${req.path} route was not found on this server`,
+    });
+  });
 
   router.use('/api', apiRouter);
 
