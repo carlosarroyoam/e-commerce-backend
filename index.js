eea2c715b707326f9bbc55561bd77e0717b5bd17
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-04-24 21:30:29 -0500

    chore(debug): add launch.json

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const container = require('./src/api/container');
 const application = container.resolve('app');
 
 application.start().catch((error) => {
-    console.error(error);
-    process.exit();
+  console.error(error);
+  process.exit();
 });
