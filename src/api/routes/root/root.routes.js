64bd9ae2e67968669fa2e316299f732681293e9c
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-04-30 21:07:44 -0500

    refactor(root route): add user resource information

--- src/api/routes/root/root.routes.js
+++ src/api/routes/root/root.routes.js
@@ -4,10 +4,26 @@ module.exports = () => {
   const router = Router();
 
   router.get('/', (request, response) => {
+    const fullUrl = `${request.protocol}://${request.hostname}:3000${request.originalUrl}`;
+
     response.send({
       version: '0.1.0-snapshot',
       documentation: 'https://api.carlosarroyo.com/docs',
       author: 'carlosarroyoam',
+      resources: {
+        users: {
+          paths: [
+            {
+              name: 'index',
+              path: `${fullUrl}api/user`,
+            },
+            {
+              name: 'show',
+              path: `${fullUrl}api/user/[id]`,
+            },
+          ],
+        },
+      },
     });
   });
 
