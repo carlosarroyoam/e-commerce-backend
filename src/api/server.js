05acf936bf0751bc7d8ca769eea29aab47713047
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-05-12 11:42:21 -0500

    chore(logging): add basic error logging

--- src/api/server.js
+++ src/api/server.js
@@ -15,6 +15,8 @@ class Server {
       .use(morgan('dev'))
       .use(router)
       .use((err, req, res, next) => {
+        console.error(err.message);
+
         if (err.status === 404) {
           res.status(404).send({
             error: err.name,
