b72cc3b389e1beced1f49145bf34b7a5d5fbce8d
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-05-16 14:17:37 -0500

    chore(database): update database script

--- node-api.sql
+++ node-api.sql
@@ -10,12 +10,12 @@ CREATE TABLE `users` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT NOW(),
-  `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-  `deleted_at` timestamp,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `EMAIL` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -24,7 +24,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Carlos Alberto','Arroyo Martínez','carlosarroyoam@gmail.com'),(2,'Cathy Stefania','Guido Rojas','stefaniaguido@gmail.com'),(5,'Juanito','López','juanlopez@gmail.com');
+INSERT INTO `users`(`id`, `first_name`, `last_name`, `email`) VALUES (1,'Carlos Alberto','Arroyo Martínez','carlosarroyo@gmail.com'),(2,'Stefania','Guido Rojas','stefaniaguido@gmail.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
