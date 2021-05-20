d949ee87e88514691f1da4a8ec532d7b064f2c06
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-05-20 08:13:41 -0500

    chore(defaultRoute): update keys order on response

--- src/api/routes/default.routes.js
+++ src/api/routes/default.routes.js
@@ -5,8 +5,8 @@ module.exports = () => {
 
   router.get('*', (request, response) => {
     response.status(404).send({
-      status: 'Not Found',
       message: `The ${request.originalUrl} route was not found on this server`,
+      error: 'Not found',
     });
   });
 
