e1b91b305c44c4e4ff1ea1eb52d0a629232b4724
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-05-22 11:40:33 -0500

    chore(logger): change logs directory

--- src/lib/winston/logger.js
+++ src/lib/winston/logger.js
@@ -15,8 +15,8 @@ class Logger {
         // - Write all logs with level `error` and below to `error.log`
         // - Write all logs with level `info` and below to `combined.log`
         //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
       ],
     });
 
