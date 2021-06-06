31fdb23d84d72216b0e03e1f9e312308fd2c25b4
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-06-06 11:40:33 -0500

    chore(docs): add docs for UserDao class

--- src/modules/users/dao/user.dao.js
+++ src/modules/users/dao/user.dao.js
@@ -1,8 +1,22 @@
+/**
+ * This class contains methods for user management in
+ * the database.
+ */
 class UserDao {
+  /**
+   * Constructor for UserDao.
+   *
+   * @param {*} connection The actual connection
+   */
   constructor(connection) {
     this.connection = connection;
   }
 
+  /**
+  * Performs the SQL query to get all non-deleted/active users.
+  *
+  * @returns {Promise}
+  */
   async getAll() {
     const query = `SELECT id, first_name, last_name, email, userable_type, userable_id, created_at, updated_at
       FROM user
@@ -11,6 +25,11 @@ class UserDao {
     return this.connection.query(query);
   }
 
+  /**
+  * Performs the SQL query to get all deleted/inactive users.
+  *
+  * @returns {Promise}
+  */
   async getTrashed() {
     const query = `SELECT id, first_name, last_name, email, userable_type, userable_id, created_at, updated_at
       FROM user
@@ -19,6 +38,11 @@ class UserDao {
     return this.connection.query(query);
   }
 
+  /**
+  * Performs the SQL query to get all users.
+  *
+  * @returns {Promise}
+  */
   async getAllWithTrashed() {
     const query = `SELECT id, first_name, last_name, email, userable_type, userable_id, created_at, updated_at
       FROM user`;
@@ -26,6 +50,11 @@ class UserDao {
     return this.connection.query(query);
   }
 
+  /**
+  * Performs the SQL query to get a non-deleted/active user by its id.
+  *
+  * @returns {Promise}
+  */
   async getById(id) {
     const query = `SELECT id, first_name, last_name, email, userable_type, userable_id, created_at, updated_at
       FROM user
@@ -34,6 +63,11 @@ class UserDao {
     return this.connection.query(query, [id]);
   }
 
+  /**
+  * Performs the SQL query to get a deleted/inactive user by its id.
+  *
+  * @returns {Promise}
+  */
   async getTrashedById(id) {
     const query = `SELECT id, first_name, last_name, email, userable_type, userable_id, created_at, updated_at
       FROM user
@@ -42,42 +76,66 @@ class UserDao {
     return this.connection.query(query, [id]);
   }
 
+  /**
+  * Performs the SQL query to get a non-deleted/active user by its email address.
+  *
+  * @returns {Promise}
+  */
   async getByEmail(email) {
     const query = 'SELECT id, email, password FROM user WHERE email = ? AND deleted_at IS NULL';
 
     return this.connection.query(query, [email]);
   }
 
-  async getByEmailForLogin(email) {
-    const query = 'SELECT id, email, password FROM user WHERE email = ? AND deleted_at IS NULL';
-
-    return this.connection.query(query, [email]);
-  }
-
+  /**
+  * Performs the SQL query to get a user by its email address.
+  *
+  * @returns {Promise}
+  */
   async getByEmailWithTrashed(email) {
     const query = 'SELECT id FROM user WHERE email = ?';
 
     return this.connection.query(query, [email]);
   }
 
+  /**
+  * Performs the SQL query to insert a user.
+  *
+  * @returns {Promise}
+  */
   async create(user) {
     const query = 'INSERT INTO user SET ?';
 
     return this.connection.query(query, [user]);
   }
 
+  /**
+  * Performs the SQL query to update a user.
+  *
+  * @returns {Promise}
+  */
   async update(userId, user) {
     const query = 'UPDATE user SET ? WHERE id = ? LIMIT 1';
 
     return this.connection.query(query, [user, userId]);
   }
 
+  /**
+  * Performs the SQL query to set a deleted/inactive state to a user.
+  *
+  * @returns {Promise}
+  */
   async delete(userId) {
     const query = 'UPDATE user SET deleted_at = CURRENT_TIMESTAMP WHERE id = ? LIMIT 1';
 
     return this.connection.query(query, [userId]);
   }
 
+  /**
+  * Performs the SQL query to set a non-deleted/active state to a user.
+  *
+  * @returns {Promise}
+  */
   async restore(userId) {
     const query = 'UPDATE user SET deleted_at = NULL WHERE id = ? LIMIT 1';
 
