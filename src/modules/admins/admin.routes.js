0a4183b6b11385dcd5013b0898ba0eb413109f6a
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-06-09 14:07:20 -0500

    chore(middleware): move validateRequestMiddleware

--- src/modules/admins/admin.routes.js
+++ src/modules/admins/admin.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-const validateMiddleware = require('../../api/middlewares/validateRequest.middleware');
+const validateMiddleware = require('../core/middlewares/validateRequest.middleware');
 const createAdminDto = require('./dtos/create.dto');
 const updateAdminDto = require('./dtos/update.dto');
 
-module.exports = ({ adminController, verifyJwtMiddleware }) => {
+module.exports = ({ adminController, verifyTokenMiddleware }) => {
   const router = Router();
 
-  router.get('/', verifyJwtMiddleware, adminController.index.bind(adminController));
+  router.get('/', verifyTokenMiddleware, adminController.index.bind(adminController));
   router.get('/:id', adminController.show.bind(adminController));
   router.post('/', validateMiddleware(createAdminDto), adminController.store.bind(adminController));
   router.put('/:id', validateMiddleware(updateAdminDto), adminController.update.bind(adminController));
