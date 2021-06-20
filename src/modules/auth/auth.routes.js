856a8f075370eab469029637ffa082a12183dd79
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-06-20 12:34:45 -0500

    add login request validation

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const validateMiddleware = require('../core/middlewares/validateRequest.middleware');
+const loginDto = require('./dtos/login.dto');
 
 module.exports = ({ authController }) => {
   const router = Router();
 
-  router.post('/login', authController.login.bind(authController));
+  router.post('/login', validateMiddleware(loginDto), authController.login.bind(authController));
 
   return router;
 };
---
+++ src/modules/auth/dtos/login.dto.js
@@ -0,0 +1,4 @@
+module.exports = {
+  email: ['required', 'email'],
+  password: ['required', 'min:8'],
+};
