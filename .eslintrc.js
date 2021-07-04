bf0540f240e206b764c9bffaaf3b8e430117953c
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-07-03 19:35:13 -0500

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         es2021: true,
         node: true,
     },
+    parser: 'babel-eslint',
     plugins: ['security'],
     extends: ['google', 'prettier', 'plugin:security/recommended'],
     parserOptions: {
