6ae9e78d03135128ca4805c6d8130c0333936688
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-07-07 08:31:57 -0500

    update database script

--- nodejs-api.sql
+++ nodejs-api.sql
@@ -39,16 +39,15 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_idx` (`email`),
   FOREIGN KEY (user_role_id)
-        REFERENCES user_roles(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    REFERENCES user_roles(id)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `user_role_id`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'Carlos Alberto', 'Arroyo Martínez', 'carlosarroyo@gmail.com', '$2b$10$vNVtCVv7IxX1Q9Whwb//ie6SZROFY4IYcDOSn146SWph8UBEzSYte', 1, '2021-07-02 14:39:11', '2021-07-02 14:51:56', NULL);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `user_role_id`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'Carlos Alberto', 'Arroyo Martínez', 'carlosarroyo@gmail.com', '$2b$10$vNVtCVv7IxX1Q9Whwb//ie6SZROFY4IYcDOSn146SWph8UBEzSYte', 1, now(), now(), NULL);
 
 --
 -- Table structure for table `customers`
@@ -61,9 +60,8 @@ CREATE TABLE `customers` (
   `user_id` bigint UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    REFERENCES users(id)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
@@ -77,10 +75,9 @@ CREATE TABLE `admins` (
 	`is_super` tinyint NOT NULL DEFAULT 0,
 	`user_id` bigint UNSIGNED NOT NULL,
 	PRIMARY KEY (`id`),
-	  FOREIGN KEY (user_id)
-			REFERENCES users(id)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
@@ -98,15 +95,13 @@ DROP TABLE IF EXISTS `personal_access_tokens`;
 CREATE TABLE `personal_access_tokens` (
 	`id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
 	`token` varchar(64) NOT NULL,
-	`abilities` text NOT NULL,
 	`last_used_at` timestamp NULL DEFAULT NULL,
     `user_id` bigint UNSIGNED NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `deleted_at` timestamp NULL DEFAULT NULL,
 	PRIMARY KEY (`id`),
-	  FOREIGN KEY (user_id)
-			REFERENCES users(id)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
