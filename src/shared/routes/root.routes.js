9384f9286e4a883ee6ea5df5c7839a2eb986ac46
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2021-09-16 20:58:08 -0500

    remove resources from index route

--- src/shared/routes/root.routes.js
+++ src/shared/routes/root.routes.js
@@ -15,32 +15,6 @@ module.exports = () => {
       version: packageJson.version,
       licence: packageJson.license,
       documentation: `${APP_URL}/api/v1/docs`,
-      resources: {
-        users: {
-          paths: [
-            {
-              name: 'index',
-              path: `${APP_URL}/api/v1/users`,
-            },
-            {
-              name: 'show',
-              path: `${APP_URL}/api/v1/users/[id]`,
-            },
-          ],
-        },
-        admins: {
-          paths: [
-            {
-              name: 'index',
-              path: `${APP_URL}/api/v1/admins`,
-            },
-            {
-              name: 'show',
-              path: `${APP_URL}/api/v1/admins/[id]`,
-            },
-          ],
-        },
-      },
     });
   });
 
