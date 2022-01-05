68063ec551d7ea7b5785be8fac378a50cdf2ecbe
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-01-05 11:29:46 -0600

    update script data dump

--- nodejs-api.sql
+++ nodejs-api.sql
@@ -49,11 +49,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-<<<<<<< HEAD
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `user_role_id`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'Carlos Alberto', 'Arroyo Martínez', 'carlos.arroyo@bookstore.com', '$2b$10$vNVtCVv7IxX1Q9Whwb//ie6SZROFY4IYcDOSn146SWph8UBEzSYte', 1, now(), now(), NULL);
-=======
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `user_role_id`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'Carlos Alberto', 'Arroyo Martínez', 'carlosarroyo@bookstore.com', '$2b$10$vNVtCVv7IxX1Q9Whwb//ie6SZROFY4IYcDOSn146SWph8UBEzSYte', 1, now(), now(), NULL);
->>>>>>> b67cb32af6178f0620bdea602e22efb3adf8532c
 
 --
 -- Table structure for table `customers`
