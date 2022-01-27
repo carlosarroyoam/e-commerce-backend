08970c8c76c0240019ac8b9fad554b5a12cbbdc3
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-01-27 15:37:10 -0600

    update error objects

--- src/shared/docs/components/responses.js
+++ src/shared/docs/components/responses.js
@@ -7,6 +7,7 @@ module.exports = {
           schema: {
             example: {
               message: 'OK',
+              data: null,
             },
           },
         },
@@ -18,8 +19,8 @@ module.exports = {
         'application/json': {
           schema: {
             example: {
-              message: 'The request data is not valid',
               error: 'Bad request',
+              message: 'The request is not valid',
             },
           },
         },
@@ -31,8 +32,8 @@ module.exports = {
         'application/json': {
           schema: {
             example: {
-              message: 'The /api/v1/wrong-route route was not found on this server',
               error: 'Not found',
+              message: 'The /api/v1/wrong-route route was not found on this server',
             },
           },
         },
@@ -44,8 +45,8 @@ module.exports = {
         'application/json': {
           schema: {
             example: {
-              message: 'Failed to authorize user with the email: example@domain.com',
               error: 'Unauthorized',
+              message: 'Failed to authorize user with the email: example@domain.com',
             },
           },
         },
@@ -57,8 +58,8 @@ module.exports = {
         'application/json': {
           schema: {
             example: {
-              message: 'The user has not permission to perform this action',
               error: 'Forbidden',
+              message: 'The user has not permission to perform this action',
             },
           },
         },
@@ -70,8 +71,8 @@ module.exports = {
         'application/json': {
           schema: {
             example: {
-              message: 'The request data is not valid',
               error: 'Unprocessable entity',
+              message: 'The request data is not valid',
               data: {
                 password: 'The password must be between 8 and 16 characters',
                 email: 'The email format is invalid',
@@ -80,15 +81,15 @@ module.exports = {
           },
         },
       },
-    },
-    INTERNAL_SERVER_ERROR: {
-      description: 'INTERNAL_SERVER_ERROR',
-      content: {
-        'application/json': {
-          schema: {
-            example: {
-              message: 'Error while updating information',
-              error: 'Internal server error',
+      INTERNAL_SERVER_ERROR: {
+        description: 'INTERNAL_SERVER_ERROR',
+        content: {
+          'application/json': {
+            schema: {
+              example: {
+                error: 'Internal server error',
+                message: 'Error while updating information',
+              },
             },
           },
         },
