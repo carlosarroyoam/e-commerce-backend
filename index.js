15b547034ce9fa688d040ffe290f1f12dc0bb0cd
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-02-23 11:06:11 -0600

    add name to start function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./src/api/server');
 const logger = require('./src/shared/lib/winston/logger');
 
-server().catch((err) => {
+server.start().catch((err) => {
   logger.log({
     level: 'error',
     message: err.message,
--- src/api/server.js
+++ src/api/server.js
@@ -8,49 +8,51 @@ const config = require('../config');
 const router = require('./router');
 const logger = require('../shared/lib/winston/logger');
 
-module.exports = () => {
-  const app = express();
+module.exports = {
+  start: () => {
+    const app = express();
 
-  app
-    .use(cors({ origin: 'http://localhost:3001', credentials: true }))
-    .use(express.json())
-    .use(compression())
-    .use(morgan('dev'))
-    .use(helmet())
-    .use(router())
-    .use((err, req, res, next) => {
-      logger.log({
-        level: err.status ? 'info' : 'error',
-        message: err.message,
-      });
-
-      // @ts-ignore
-      if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        return res.status(400).json({
-          error: 'Bad request',
-          message: 'The JSON payload is malformed',
-        });
-      }
-
-      if (!err.fatal) {
-        return res.status(err.status || 500).json({
-          error: err.name !== 'Error' ? err.name : 'Internal server error',
+    app
+      .use(cors({ origin: 'http://localhost:3001', credentials: true }))
+      .use(express.json())
+      .use(compression())
+      .use(morgan('dev'))
+      .use(helmet())
+      .use(router())
+      .use((err, req, res, next) => {
+        logger.log({
+          level: err.status ? 'info' : 'error',
           message: err.message,
-          data: err.errors,
         });
-      }
 
-      process.exit(5);
-    });
+        // @ts-ignore
+        if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+          return res.status(400).json({
+            error: 'Bad request',
+            message: 'The JSON payload is malformed',
+          });
+        }
 
-  return new Promise((resolve) => {
-    app.listen(config.APP.PORT, () => {
-      logger.log({
-        level: 'info',
-        message: `Application running on: ${config.APP.URL}:${config.APP.PORT}`,
+        if (!err.fatal) {
+          return res.status(err.status || 500).json({
+            error: err.name !== 'Error' ? err.name : 'Internal server error',
+            message: err.message,
+            data: err.errors,
+          });
+        }
+
+        process.exit(5);
       });
 
-      resolve();
+    return new Promise((resolve) => {
+      app.listen(config.APP.PORT, () => {
+        logger.log({
+          level: 'info',
+          message: `Application running on: ${config.APP.URL}:${config.APP.PORT}`,
+        });
+
+        resolve();
+      });
     });
-  });
+  },
 };
