36698fa7ab8a1af661a0732fdc14273ac8870e16
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-03-02 08:07:58 -0600

    separate error handlers in files

--- src/api/server.js
+++ src/api/server.js
@@ -7,6 +7,8 @@ const helmet = require('helmet');
 const config = require('../config');
 const router = require('./router');
 const logger = require('../shared/lib/winston/logger');
+const validateJsonPayloadMiddleware = require('../shared/middlewares/validateJsonPayload.middleware');
+const errorHandlerMiddleware = require('../shared/middlewares/errorHandler.middleware');
 
 module.exports = {
   start: () => {
@@ -19,30 +21,8 @@ module.exports = {
       .use(morgan('dev'))
       .use(helmet())
       .use(router())
-      .use((err, req, res, next) => {
-        logger.log({
-          level: err.status !== 500 ? 'info' : 'error',
-          message: err.message,
-        });
-
-        // @ts-ignore
-        if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-          return res.status(400).json({
-            error: 'Bad request',
-            message: 'The JSON payload is malformed',
-          });
-        }
-
-        if (!err.fatal) {
-          return res.status(err.status || 500).json({
-            error: err.name,
-            message: err.message,
-            data: err.errors,
-          });
-        }
-
-        process.exit(5);
-      });
+      .use(validateJsonPayloadMiddleware())
+      .use(errorHandlerMiddleware());
 
     return new Promise((resolve) => {
       app.listen(config.APP.PORT, () => {
