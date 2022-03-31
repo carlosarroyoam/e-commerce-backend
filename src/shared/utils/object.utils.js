3a0b6946fc3beb7eff1a699b2a54e0d1e7fffbe1
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-03-31 13:18:12 -0600

    remove unused parameter

--- src/shared/utils/object.utils.js
+++ src/shared/utils/object.utils.js
@@ -2,10 +2,9 @@
  * Remove keys with undefined values from an object.
  *
  * @param {object} object The object to remove undefined.
- * @param {array} ignoreKeys The keys to ignore.
  * @return {object} The object without undefined.
  */
-const removeUndefined = (object, ignoreKeys = undefined) => {
+const removeUndefined = (object) => {
   const cleanedObject = Object.keys(object).reduce((acc, key) => {
     const _acc = acc;
     if (object[key] !== undefined) _acc[key] = object[key];
