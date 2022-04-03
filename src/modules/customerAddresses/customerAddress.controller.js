5ff980682c0fa7c6d10f415a9c1efada728ea473
unknown <carlosarroyoam@gmail.com>
2022-04-02 22:09:31 -0600

    changed customer addresses to classes

--- src/modules/customerAddresses/customerAddress.controller.js
+++ src/modules/customerAddresses/customerAddress.controller.js
@@ -2,77 +2,69 @@ const customerAddressService = require('./customerAddress.service');
 const customerAddressMapper = require('./customerAddress.mapper');
 
 /**
- * Handles incoming request from the /customers/:customer_id/addresses endpoint.
- *
- * @param {*} request The express.js request object.
- * @param {*} response The express.js response object.
- * @param {*} next The express.js next object.
+ * CustomerController class.
  */
-const index = async (request, response, next) => {
-  try {
-    const { customer_id } = request.params;
+class CustomerAddressController {
+  /**
+   * Handles incoming request from the /customers/:customer_id/addresses endpoint.
+   *
+   * @param {*} request The express.js request object.
+   * @param {*} response The express.js response object.
+   * @param {*} next The express.js next object.
+   */
+  async index(request, response, next) {
+    try {
+      const { customer_id } = request.params;
 
-    const customerAddresses = await customerAddressService.findAll(customer_id);
+      const customerAddresses = await customerAddressService.findAll(customer_id);
 
-    const customerAddressesDto = customerAddresses.map((address) =>
-      customerAddressMapper.toDto(address)
-    );
+      const customerAddressesDto = customerAddresses.map((address) =>
+        customerAddressMapper.toDto(address)
+      );
 
-    response.json({
-      message: 'Ok',
-      data: customerAddressesDto,
-    });
-  } catch (error) {
-    next(error);
+      response.json({
+        message: 'Ok',
+        data: customerAddressesDto,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-};
 
-/**
- * Handles incoming request from the /customers/:customer_id/addresses/:address_id endpoint.
- *
- * @param {*} request The express.js request object.
- * @param {*} response The express.js response object.
- * @param {*} next The express.js next object.
- */
-const show = async (request, response, next) => {
-  try {
-    const { customer_id, address_id } = request.params;
+  /**
+   * Handles incoming request from the /customers/:customer_id/addresses/:address_id endpoint.
+   *
+   * @param {*} request The express.js request object.
+   * @param {*} response The express.js response object.
+   * @param {*} next The express.js next object.
+   */
+  async show(request, response, next) {
+    try {
+      const { customer_id, address_id } = request.params;
 
-    const customerAddress = await customerAddressService.findById(customer_id, address_id);
-    const customerAddressDto = customerAddressMapper.toDto(customerAddress);
+      const customerAddress = await customerAddressService.findById(customer_id, address_id);
+      const customerAddressDto = customerAddressMapper.toDto(customerAddress);
 
-    response.json({
-      message: 'Ok',
-      data: customerAddressDto,
-    });
-  } catch (error) {
-    next(error);
+      response.json({
+        message: 'Ok',
+        data: customerAddressDto,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-};
 
-/**
- * Handles incoming request from the /customers/:customer_id/addresses endpoint.
- *
- * @param {*} request The express.js request object.
- * @param {*} response The express.js response object.
- * @param {*} next The express.js next object.
- */
-const store = async (request, response, next) => {
-  try {
-    const { customer_id } = request.params;
-    const {
-      street_name,
-      street_number,
-      apartament_number,
-      sublocality,
-      locality,
-      state,
-      postal_code,
-      phone_number,
-    } = request.body;
-
-    const createdAddress = await customerAddressService.store(
-      {
+  /**
+   * Handles incoming request from the /customers/:customer_id/addresses endpoint.
+   *
+   * @param {*} request The express.js request object.
+   * @param {*} response The express.js response object.
+   * @param {*} next The express.js next object.
+   */
+  async store(request, response, next) {
+    try {
+      const { customer_id } = request.params;
+      const {
         street_name,
         street_number,
         apartament_number,
@@ -81,43 +73,44 @@ const store = async (request, response, next) => {
         state,
         postal_code,
         phone_number,
-      },
-      customer_id
-    );
+      } = request.body;
 
-    const createdAddressDto = customerAddressMapper.toDto(createdAddress);
+      const createdAddress = await customerAddressService.store(
+        {
+          street_name,
+          street_number,
+          apartament_number,
+          sublocality,
+          locality,
+          state,
+          postal_code,
+          phone_number,
+        },
+        customer_id
+      );
 
-    response.status(201).json({
-      message: 'Ok',
-      data: createdAddressDto,
-    });
-  } catch (error) {
-    next(error);
+      const createdAddressDto = customerAddressMapper.toDto(createdAddress);
+
+      response.status(201).json({
+        message: 'Ok',
+        data: createdAddressDto,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-};
 
-/**
- * Handles incoming request from the /customers/:customer_id/addresses/:address_id endpoint.
- *
- * @param {*} request The express.js request object.
- * @param {*} response The express.js response object.
- * @param {*} next The express.js next object.
- */
-const update = async (request, response, next) => {
-  try {
-    const { customer_id, address_id } = request.params;
-    const {
-      street_name,
-      street_number,
-      apartament_number,
-      sublocality,
-      locality,
-      state,
-      postal_code,
-    } = request.body;
-
-    const updatedAddress = await customerAddressService.update(
-      {
+  /**
+   * Handles incoming request from the /customers/:customer_id/addresses/:address_id endpoint.
+   *
+   * @param {*} request The express.js request object.
+   * @param {*} response The express.js response object.
+   * @param {*} next The express.js next object.
+   */
+  async update(request, response, next) {
+    try {
+      const { customer_id, address_id } = request.params;
+      const {
         street_name,
         street_number,
         apartament_number,
@@ -125,50 +118,56 @@ const update = async (request, response, next) => {
         locality,
         state,
         postal_code,
-      },
-      customer_id,
-      address_id
-    );
+      } = request.body;
 
-    const updatedAddressDto = customerAddressMapper.toDto(updatedAddress);
+      const updatedAddress = await customerAddressService.update(
+        {
+          street_name,
+          street_number,
+          apartament_number,
+          sublocality,
+          locality,
+          state,
+          postal_code,
+        },
+        customer_id,
+        address_id
+      );
 
-    response.json({
-      message: 'Ok',
-      data: updatedAddressDto,
-    });
-  } catch (error) {
-    next(error);
+      const updatedAddressDto = customerAddressMapper.toDto(updatedAddress);
+
+      response.json({
+        message: 'Ok',
+        data: updatedAddressDto,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-};
 
-/**
- * Handles incoming request from the /users/:user_id endpoint.
- *
- * @param {*} request The express.js request object.
- * @param {*} response The express.js response object.
- * @param {*} next The express.js next object.
- */
-const destroy = async (request, response, next) => {
-  try {
-    const { customer_id, address_id } = request.params;
+  /**
+   * Handles incoming request from the /users/:user_id endpoint.
+   *
+   * @param {*} request The express.js request object.
+   * @param {*} response The express.js response object.
+   * @param {*} next The express.js next object.
+   */
+  async destroy(request, response, next) {
+    try {
+      const { customer_id, address_id } = request.params;
 
-    const addressDeletedId = await customerAddressService.deleteById(customer_id, address_id);
+      const addressDeletedId = await customerAddressService.deleteById(customer_id, address_id);
 
-    response.json({
-      message: 'The address was successfully deleted',
-      data: {
-        user_deleted_id: addressDeletedId,
-      },
-    });
-  } catch (error) {
-    next(error);
+      response.json({
+        message: 'The address was successfully deleted',
+        data: {
+          user_deleted_id: addressDeletedId,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-};
+}
 
-module.exports = {
-  index,
-  show,
-  store,
-  update,
-  destroy,
-};
+module.exports = new CustomerAddressController();
--- src/modules/customers/customer.service.js
+++ src/modules/customers/customer.service.js
@@ -1,6 +1,6 @@
 const dbConnectionPool = require('../../shared/lib/mysql/connectionPool');
 const CustomerRepository = require('./customer.repository');
-const customerAddressRepository = require('../customerAddresses/customerAddress.repository');
+const CustomerAddressRepository = require('../customerAddresses/customerAddress.repository');
 const UserRepository = require('../users/user.repository');
 const sharedErrors = require('../../shared/errors');
 const userRoles = require('../auth/roles');
@@ -8,224 +8,225 @@ const bcrypt = require('../../shared/lib/bcrypt');
 const logger = require('../../shared/lib/winston/logger');
 
 /**
- * Retrieves all customer users.
- *
- * @param {object} queryOptions The query options.
- * @param {number} queryOptions.skip The query skip.
- * @param {number} queryOptions.limit The query limit.
- * @param {string} queryOptions.sort The order for the results.
- * @param {string} queryOptions.status The user status to query.
- * @param {string} queryOptions.search The search criteria.
- * @return {Promise} The list of customers.
+ * CustomerService class.
  */
-const findAll = async ({ skip, limit, sort, status, search }) => {
-  let connection;
+class CustomerService {
+  /**
+   * Retrieves all customer users.
+   *
+   * @param {object} queryOptions The query options.
+   * @param {number} queryOptions.skip The query skip.
+   * @param {number} queryOptions.limit The query limit.
+   * @param {string} queryOptions.sort The order for the results.
+   * @param {string} queryOptions.status The user status to query.
+   * @param {string} queryOptions.search The search criteria.
+   * @return {Promise} The list of customers.
+   */
+  async findAll({ skip, limit, sort, status, search }) {
+    let connection;
 
-  try {
-    connection = await dbConnectionPool.getConnection();
-    const customerRepository = new CustomerRepository(connection);
+    try {
+      connection = await dbConnectionPool.getConnection();
+      const customerRepository = new CustomerRepository(connection);
+      const customerAddressRepository = new CustomerAddressRepository(connection);
 
-    const rawCustomers = await customerRepository.findAll({ skip, limit, sort, status, search });
+      const rawCustomers = await customerRepository.findAll({ skip, limit, sort, status, search });
 
-    const customers = await Promise.all(
-      rawCustomers.map(async (customer) => {
-        const addressesByCustomerId = await customerAddressRepository.findByCustomerId(
-          customer.id,
-          connection
-        );
+      const customers = await Promise.all(
+        rawCustomers.map(async (customer) => {
+          const addressesByCustomerId = await customerAddressRepository.findByCustomerId(
+            customer.id
+          );
 
-        return {
-          ...customer,
-          addresses: addressesByCustomerId,
-        };
-      })
-    );
+          return {
+            ...customer,
+            addresses: addressesByCustomerId,
+          };
+        })
+      );
 
-    connection.release();
-
-    return customers;
-  } catch (err) {
-    if (connection) connection.release();
-
-    if (!err.status) {
-      logger.error({
-        message: err.message,
-      });
-
-      throw new sharedErrors.InternalServerError({ message: 'Error while retrieving customers' });
-    }
-
-    throw err;
-  }
-};
-
-/**
- * Retrieves a customer user by its id.
- *
- * @param {number} customer_id The id of the customer user to retrieve.
- * @return {Promise} The customer user.
- */
-const findById = async (customer_id) => {
-  let connection;
-
-  try {
-    connection = await dbConnectionPool.getConnection();
-    const customerRepository = new CustomerRepository(connection);
-
-    const customerById = await customerRepository.findById(customer_id);
-
-    if (!customerById) {
-      throw new sharedErrors.UserNotFoundError({ email: undefined });
-    }
-
-    const addressesByCustomerId = await customerAddressRepository.findByCustomerId(customer_id);
-
-    connection.release();
-
-    return {
-      ...customerById,
-      addresses: addressesByCustomerId,
-    };
-  } catch (err) {
-    if (connection) connection.release();
-
-    if (!err.status) {
-      logger.error({
-        message: err.message,
-      });
-
-      throw new sharedErrors.InternalServerError({ message: 'Error while retrieving customer' });
-    }
-
-    throw err;
-  }
-};
-
-/**
- * Stores a customer user.
- *
- * @param {object} customer The customer user to store.
- * @return {Promise} The created customer user.
- */
-const store = async (customer) => {
-  let connection;
-
-  try {
-    connection = await dbConnectionPool.getConnection();
-    const userRepository = new UserRepository(connection);
-    const customerRepository = new CustomerRepository(connection);
-
-    connection.beginTransaction();
-
-    const customerByEmail = await userRepository.findByEmail(customer.email);
-
-    if (customerByEmail) {
-      throw new sharedErrors.EmailAlreadyTakenError({
-        email: customer.email,
-      });
-    }
-
-    const passwordHash = await bcrypt.hashPassword(customer.password);
-
-    const createdUserId = await userRepository.store({
-      ...customer,
-      password: passwordHash,
-      user_role_id: userRoles.customer.id,
-    });
-
-    const createdCustomerId = await customerRepository.store({
-      user_id: createdUserId,
-    });
-
-    const createdCustomer = await customerRepository.findById(createdCustomerId);
-
-    connection.commit();
-
-    connection.release();
-
-    return createdCustomer;
-  } catch (err) {
-    if (connection) {
-      connection.rollback();
       connection.release();
+
+      return customers;
+    } catch (err) {
+      if (connection) connection.release();
+
+      if (!err.status) {
+        logger.error({
+          message: err.message,
+        });
+
+        throw new sharedErrors.InternalServerError({ message: 'Error while retrieving customers' });
+      }
+
+      throw err;
     }
-
-    if (!err.status) {
-      logger.error({
-        message: err.message,
-      });
-
-      throw new sharedErrors.InternalServerError({ message: 'Error while storing customer' });
-    }
-
-    throw err;
   }
-};
 
-/**
- * Updates a customer user by its id.
- *
- * @param {number} customer_id The id of the customer user to update.
- * @param {object} customer The customer user to store.
- * @return {Promise} The updated customer user.
- */
-const update = async (customer_id, customer) => {
-  let connection;
+  /**
+   * Retrieves a customer user by its id.
+   *
+   * @param {number} customer_id The id of the customer user to retrieve.
+   * @return {Promise} The customer user.
+   */
+  async findById(customer_id) {
+    let connection;
 
-  try {
-    connection = await dbConnectionPool.getConnection();
-    const userRepository = new UserRepository(connection);
-    const customerRepository = new CustomerRepository(connection);
+    try {
+      connection = await dbConnectionPool.getConnection();
+      const customerRepository = new CustomerRepository(connection);
+      const customerAddressRepository = new CustomerAddressRepository(connection);
 
-    connection.beginTransaction();
+      const customerById = await customerRepository.findById(customer_id);
 
-    const customerById = await customerRepository.findById(customer_id);
+      if (!customerById) {
+        throw new sharedErrors.UserNotFoundError({ email: undefined });
+      }
 
-    if (!customerById) {
-      throw new sharedErrors.UserNotFoundError({ email: undefined });
+      const addressesByCustomerId = await customerAddressRepository.findByCustomerId(customer_id);
+
+      connection.release();
+
+      return {
+        ...customerById,
+        addresses: addressesByCustomerId,
+      };
+    } catch (err) {
+      if (connection) connection.release();
+
+      if (!err.status) {
+        logger.error({
+          message: err.message,
+        });
+
+        throw new sharedErrors.InternalServerError({ message: 'Error while retrieving customer' });
+      }
+
+      throw err;
     }
+  }
 
-    if (customerById.deleted_at !== null) {
-      throw new sharedErrors.BadRequestError({
-        message: 'The user account is disabled',
-      });
-    }
+  /**
+   * Stores a customer user.
+   *
+   * @param {object} customer The customer user to store.
+   * @return {Promise} The created customer user.
+   */
+  async store(customer) {
+    let connection;
 
-    await userRepository.update(
-      {
+    try {
+      connection = await dbConnectionPool.getConnection();
+      const userRepository = new UserRepository(connection);
+      const customerRepository = new CustomerRepository(connection);
+
+      connection.beginTransaction();
+
+      const customerByEmail = await userRepository.findByEmail(customer.email);
+
+      if (customerByEmail) {
+        throw new sharedErrors.EmailAlreadyTakenError({
+          email: customer.email,
+        });
+      }
+
+      const passwordHash = await bcrypt.hashPassword(customer.password);
+
+      const createdUserId = await userRepository.store({
         ...customer,
-      },
-      customerById.user_id
-    );
-
-    const updatedCustomer = await customerRepository.findById(customer_id);
-
-    connection.commit();
-
-    connection.release();
-
-    return updatedCustomer;
-  } catch (err) {
-    if (connection) {
-      connection.rollback();
-      connection.release();
-    }
-
-    if (!err.status) {
-      logger.error({
-        message: err.message,
+        password: passwordHash,
+        user_role_id: userRoles.customer.id,
       });
 
-      throw new sharedErrors.InternalServerError({ message: 'Error while updating customer' });
+      const createdCustomerId = await customerRepository.store({
+        user_id: createdUserId,
+      });
+
+      const createdCustomer = await customerRepository.findById(createdCustomerId);
+
+      connection.commit();
+
+      connection.release();
+
+      return createdCustomer;
+    } catch (err) {
+      if (connection) {
+        connection.rollback();
+        connection.release();
+      }
+
+      if (!err.status) {
+        logger.error({
+          message: err.message,
+        });
+
+        throw new sharedErrors.InternalServerError({ message: 'Error while storing customer' });
+      }
+
+      throw err;
     }
-
-    throw err;
   }
-};
 
-module.exports = {
-  findAll,
-  findById,
-  store,
-  update,
-};
+  /**
+   * Updates a customer user by its id.
+   *
+   * @param {number} customer_id The id of the customer user to update.
+   * @param {object} customer The customer user to store.
+   * @return {Promise} The updated customer user.
+   */
+  async update(customer_id, customer) {
+    let connection;
+
+    try {
+      connection = await dbConnectionPool.getConnection();
+      const userRepository = new UserRepository(connection);
+      const customerRepository = new CustomerRepository(connection);
+
+      connection.beginTransaction();
+
+      const customerById = await customerRepository.findById(customer_id);
+
+      if (!customerById) {
+        throw new sharedErrors.UserNotFoundError({ email: undefined });
+      }
+
+      if (customerById.deleted_at !== null) {
+        throw new sharedErrors.BadRequestError({
+          message: 'The user account is disabled',
+        });
+      }
+
+      await userRepository.update(
+        {
+          ...customer,
+        },
+        customerById.user_id
+      );
+
+      const updatedCustomer = await customerRepository.findById(customer_id);
+
+      connection.commit();
+
+      connection.release();
+
+      return updatedCustomer;
+    } catch (err) {
+      if (connection) {
+        connection.rollback();
+        connection.release();
+      }
+
+      if (!err.status) {
+        logger.error({
+          message: err.message,
+        });
+
+        throw new sharedErrors.InternalServerError({ message: 'Error while updating customer' });
+      }
+
+      throw err;
+    }
+  }
+}
+
+module.exports = new CustomerService();
