879e9f053f1d6d4bdd711f512fdf841815977344
unknown <carlosarroyoam@gmail.com>
2022-04-03 19:00:58 -0500

    change admin to classes

--- src/modules/admins/admin.dao.js
+++ src/modules/admins/admin.dao.js
@@ -1,17 +1,29 @@
 /**
- * Performs the SQL query to get all admin users.
- *
- * @param {object} queryOptions The query options.
- * @param {number} queryOptions.skip The query skip.
- * @param {number} queryOptions.limit The query limit.
- * @param {string} queryOptions.sort The order for the results.
- * @param {string} queryOptions.status The user status to query.
- * @param {string} queryOptions.search The search criteria.
- * @param {*} connection The database connection object.
- * @return {Promise} The query result.
+ * AdminDao class.
  */
-async function getAll({ skip = 0, limit = 50, sort = 'id', status, search }, connection) {
-  let query = `SELECT
+class AdminDao {
+  /**
+   * AdminDao class constructor.
+   *
+   * @param {*} connection The database connection object.
+   */
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  /**
+   * Performs the SQL query to get all admin users.
+   *
+   * @param {object} queryOptions The query options.
+   * @param {number} queryOptions.skip The query skip.
+   * @param {number} queryOptions.limit The query limit.
+   * @param {string} queryOptions.sort The order for the results.
+   * @param {string} queryOptions.status The user status to query.
+   * @param {string} queryOptions.search The search criteria.
+   * @return {Promise} The query result.
+   */
+  async getAll({ skip = 0, limit = 50, sort = 'id', status, search }) {
+    let query = `SELECT
         adm.id,
         usr.id AS user_id,
         usr.first_name,
@@ -25,45 +37,44 @@ async function getAll({ skip = 0, limit = 50, sort = 'id', status, search }, con
     LEFT JOIN users usr ON adm.user_id = usr.id
     WHERE 1`;
 
-  if (status) {
-    if (status === 'active') {
-      query += ' AND usr.deleted_at IS NULL';
-    } else {
-      query += ' AND usr.deleted_at IS NOT NULL';
-    }
-  }
-
-  if (search) {
-    query += ` AND MATCH(first_name, last_name) AGAINST("${connection.escape(
-      search
-    )}*" IN BOOLEAN MODE)`;
-  }
-
-  if (sort) {
-    let order = 'ASC';
-
-    if (sort.charAt(0) === '-') {
-      order = 'DESC';
-      sort = sort.substring(1);
+    if (status) {
+      if (status === 'active') {
+        query += ' AND usr.deleted_at IS NULL';
+      } else {
+        query += ' AND usr.deleted_at IS NOT NULL';
+      }
     }
 
-    query += ` ORDER BY ${connection.escapeId(sort)} ${order}`;
+    if (search) {
+      query += ` AND MATCH(first_name, last_name) AGAINST("${this.connection.escape(
+        search
+      )}*" IN BOOLEAN MODE)`;
+    }
+
+    if (sort) {
+      let order = 'ASC';
+
+      if (sort.charAt(0) === '-') {
+        order = 'DESC';
+        sort = sort.substring(1);
+      }
+
+      query += ` ORDER BY ${this.connection.escapeId(sort)} ${order}`;
+    }
+
+    query += ` LIMIT ${this.connection.escape(skip)}, ${this.connection.escape(limit)}`;
+
+    return this.connection.query(query);
   }
 
-  query += ` LIMIT ${connection.escape(skip)}, ${connection.escape(limit)}`;
-
-  return connection.query(query);
-}
-
-/**
- * Performs the SQL query to get a non-deleted/active admin user by its id.
- *
- * @param {number} admin_id The id of the admin user to query.
- * @param {*} connection The database connection object.
- * @return {Promise} The query result.
- */
-async function getById(admin_id, connection) {
-  const query = `SELECT
+  /**
+   * Performs the SQL query to get a non-deleted/active admin user by its id.
+   *
+   * @param {number} admin_id The id of the admin user to query.
+   * @return {Promise} The query result.
+   */
+  async getById(admin_id) {
+    const query = `SELECT
         adm.id,
         usr.id AS user_id,
         usr.first_name,
@@ -77,18 +88,17 @@ async function getById(admin_id, connection) {
     LEFT JOIN users usr ON adm.user_id = usr.id
     WHERE adm.id = ?`;
 
-  return connection.query(query, [admin_id]);
-}
+    return this.connection.query(query, [admin_id]);
+  }
 
-/**
- * Performs the SQL query to get a non-deleted/active admin user by its email address.
- *
- * @param {string} email The email of the admin user to query.
- * @param {*} connection The database connection object.
- * @return {Promise} The query result.
- */
-async function getByEmail(email, connection) {
-  const query = `SELECT
+  /**
+   * Performs the SQL query to get a non-deleted/active admin user by its email address.
+   *
+   * @param {string} email The email of the admin user to query.
+   * @return {Promise} The query result.
+   */
+  async getByEmail(email) {
+    const query = `SELECT
         adm.id,
         usr.id AS user_id,
         usr.first_name,
@@ -102,40 +112,33 @@ async function getByEmail(email, connection) {
     LEFT JOIN users usr ON adm.user_id = usr.id
     WHERE usr.email = ?`;
 
-  return connection.query(query, [email]);
+    return this.connection.query(query, [email]);
+  }
+
+  /**
+   * Performs the SQL query to insert a admin user.
+   *
+   * @param {object} admin The admin user to store.
+   * @return {Promise} The query result.
+   */
+  async create(admin) {
+    const query = 'INSERT INTO admins SET ?';
+
+    return this.connection.query(query, [admin]);
+  }
+
+  /**
+   * Performs the SQL query to update a admin user.
+   *
+   * @param {object} admin The admin user to update.
+   * @param {number} admin_id The id of the admin user to update.
+   * @return {Promise} The query result.
+   */
+  async update(admin, admin_id) {
+    const query = 'UPDATE admins SET ? WHERE id = ? LIMIT 1';
+
+    return this.connection.query(query, [admin, admin_id]);
+  }
 }
 
-/**
- * Performs the SQL query to insert a admin user.
- *
- * @param {object} admin The admin user to store.
- * @param {*} connection The database connection object.
- * @return {Promise} The query result.
- */
-async function create(admin, connection) {
-  const query = 'INSERT INTO admins SET ?';
-
-  return connection.query(query, [admin]);
-}
-
-/**
- * Performs the SQL query to update a admin user.
- *
- * @param {object} admin The admin user to update.
- * @param {number} admin_id The id of the admin user to update.
- * @param {*} connection The database connection object.
- * @return {Promise} The query result.
- */
-async function update(admin, admin_id, connection) {
-  const query = 'UPDATE admins SET ? WHERE id = ? LIMIT 1';
-
-  return connection.query(query, [admin, admin_id]);
-}
-
-module.exports = {
-  getAll,
-  getById,
-  getByEmail,
-  create,
-  update,
-};
+module.exports = AdminDao;
