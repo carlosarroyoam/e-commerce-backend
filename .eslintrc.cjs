ded21f5657a103f559bf11daaaeae1643095a2b9
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-03-09 17:25:43 -0600

    update code format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 2021,
-		sourceType: 'module',
-	},
-	env: {
-		commonjs: true,
-		es2021: true,
-		node: true,
-	},
-	plugins: ['prettier'],
-	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'linebreak-style': ['error', 'unix | windows'],
+  },
 };
